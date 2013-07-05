b74b02d372caee73317645ade1aaeec31a4081e8
Laurent <lbnetid+rb@gmail.com>
2013-07-05 17:49:34 +0200

    Crap stuff for testing... doesn't work

--- lib/easy_app_helper/version.rb
+++ lib/easy_app_helper/version.rb
@@ -6,5 +6,5 @@
 ################################################################################
 
 module EasyAppHelper
-  EASY_APP_HELPER_VERSION = "1.0.3"
+  EASY_APP_HELPER_VERSION = "1.0.4"
 end
