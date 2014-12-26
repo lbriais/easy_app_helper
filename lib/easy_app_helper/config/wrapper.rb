b0f8fe309133275e669e4b0a989b4736e5832755
L.Briais <lbnetid+gh@gmail.com>
2014-12-26 12:39:03 +0100

    First usable version

--- lib/easy_app_helper/config/wrapper.rb
+++ lib/easy_app_helper/config/wrapper.rb
@@ -3,7 +3,12 @@ module EasyAppHelper
 
     module Wrapper
       def safely_exec(message, *args, &block)
-        raise 'Not implemented'
+        if self[:simulate]
+          EasyAppHelper.puts_and_logs "SIMULATING: #{message}" unless message.nil?
+        else
+          EasyAppHelper.puts_and_logs message
+          yield(*args)
+        end
       end
     end
 
